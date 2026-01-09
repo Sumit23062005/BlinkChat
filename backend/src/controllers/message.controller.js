14847a45d457f8723c9e1b1fd2883b12d22b3a33
Sumit Jadhav <sumitjune123@gmail.com>
2026-01-09 09:49:44 +0530

    realtime messages implemented

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,6 +1,8 @@
 import Message from "../models/message.js" ;
 import User from "../models/User.js";
 import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId } from "../lib/socket.js";
+import { io } from "../lib/socket.js";
 
 export const getAllContacts = async (req ,res) => {
     try {
@@ -70,6 +72,11 @@ export const sendMessage = async (req ,res) => {
 
         await newMessage.save() ;
         //todo : send message in real-time if user is online - socket.io 
+        const receiverSocketId = getReceiverSocketId(receiverId) ;
+        if(receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage) ;
+
+        }
         res.status(201).json(newMessage) ;
     } catch (error) {
         console.log("Error in sendMessage controller :" , error.message) ;  
